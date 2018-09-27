40b302a572fdb26e2fa822d8f783442989d00e17
smartnet <ivan@kachinskiy.ru>
2018-09-28 01:16:39 +0300

    Шаблоны и роутинг через аннотации - заработали. Но пропала веб-консоль :-(

--- src/Controller/MicroController.php
+++ src/Controller/MicroController.php
@@ -9,16 +9,18 @@
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class MicroController
+class MicroController extends AbstractController
 {
  /**
   * @Route("/")
   */
     public function number() {
         $number = random_int(0, 100);
-        return new Response(
-          '<html><body>Счастливое число: '.$number.'</body></html>'
-        );
+        return $this->render(
+            'micro/number.html.twig', [
+            'number' => $number
+        ]);
     }
 }
